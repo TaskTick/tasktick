6c1ed5a94fc4eabb9c426d68700ff075996f1450
HaythamT95 <haythamtaweel95@gmail.com>
2023-03-13 17:23:37 +0200

    Multilanguage support

---
+++ patient/translations/SignInTranslation.js
@@ -0,0 +1,23 @@
+const translation = {
+    en:{
+        welcome:"Welcome",
+        login:"Login to your account",
+        username:"Username",
+        password:"Password",
+        loginSubmit:"Login",
+        forgotPass:"Forgot Password?",
+        empty:"Please fill out all required fields",
+        wrongPass:"Wrong username or password"
+    },
+    he:{
+        welcome:"ברוכים הבאים",
+        login:"התחבר לחשבון שלך",
+        username:"שם משתמש",
+        password:"סיסמה",
+        loginSubmit:"התחבר",
+        forgotPass:"שכחת סיסמה?",
+        empty:"בבקשה הכנס את כל הפרטים",
+        wrongPass:"שם משתמש או סיסמה שגויה"
+    }
+}
+export default translation
